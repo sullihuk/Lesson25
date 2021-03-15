ed38fd9d1e89fe4855ca1ff1a18054de65af919c
tdds <tdds@localhost.localdomain>
2021-03-16 05:31:19 +0700

    add to db, read from db

--- app.rb
+++ app.rb
@@ -2,3 +2,10 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'machine.db'
 
+#db.execute "INSERT INTO Machine (NAME, PRICE) VALUES ('TOKARN', '35000')"
+
+db.execute "SELECT * FROM Machine" do |ones|
+	puts ones
+	puts "===="
+end
+db.close
